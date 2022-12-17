7a2a23f45b7c195486e59de3555c1cc88170d01c
yenjia <rex19981002@gmail.com>
2022-12-17 22:41:24 +0800

    add reproduce, config file and datalist

---
+++ command/reproduce.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source infer_public.sh ../config/regnet.yaml
+source infer_public.sh ../config/swin_s_1080.yaml
+source infer_public.sh ../config/swin_b.yaml
+source infer_public.sh ../config/swin_b_1080.yaml
